52084177ddc99cd4af22aaca3c8290cc4bef5e91
meghanaredie <38308691+meghanaredie@users.noreply.github.com>
2019-09-10 13:38:05 +0530

    Update and rename README.md to check if one no. is between given two no.s

---
+++ check if one no. is between given two no.s
@@ -0,0 +1,15 @@
+N=int(input())
+L,R=map(int,input().split())
+if N in range(L,R+1):
+    print("yes")
+else:
+    print("no")
+    
+    
+    \\Given a number N and 2 numbers L and R. Check whether N is between L and R. Print yes/no.
+Sample Testcase :
+INPUT
+3
+2 6
+OUTPUT
+yes\\\
